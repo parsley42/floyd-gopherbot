0498026f6aa58f16893f8f00dfbef1c65e421167
David Parsley <parsley@linuxjedi.org>
2018-07-28 09:52:48 +0000

    Initial commit

---
+++ jobs/githubci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# githubci.sh - a Bash job triggered by github commits
+
+GITHUB_REPOSITORY=$1
+GITHUB_BRANCH=$2
+shift 2
+
+source $GOPHER_INSTALLDIR/lib/gopherbot_v1.sh
+
+Say "Hey! I see there's a new commit to '$GITHUB_REPOSITORY' in the '$GITHUB_BRANCH' branch. Someday I'm going to do something about that!"
