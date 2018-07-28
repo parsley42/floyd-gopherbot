bdb30515b4edbc5a3de1b0f0402b4691263e990c
David Parsley <parsley@linuxjedi.org>
2018-07-28 07:43:48 -0400

    Update githubci.sh
    Checking to see if Floyd will update himself

--- jobs/githubci.sh
+++ jobs/githubci.sh
@@ -8,4 +8,4 @@ shift 2
 
 source $GOPHER_INSTALLDIR/lib/gopherbot_v1.sh
 
-Say "Hey! I see there's a new commit to '$GITHUB_REPOSITORY' in the '$GITHUB_BRANCH' branch. Someday I'm going to do something about that!"
+Say "Hey! I see there's a new commit to '$GITHUB_REPOSITORY' in the '$GITHUB_BRANCH' branch. Gonna do something about that real soon!"
