dc071cd018e97e926080ad929f8787cfb146f039
David Parsley <parsley@linuxjedi.org>
2023-05-06 11:47:55 +0000

    WIP rewrite

--- plugins/ai.rb
+++ plugins/ai.rb
@@ -11,108 +11,33 @@ command = ARGV.shift()
 
 defaultConfig = <<'DEFCONFIG'
 ---
-## n.b. All of this can be overridden with custom config in
-## conf/plugins/<pluginname>.yaml. Hashes are merged with custom
-## config taking precedence. Arrays can be overwritten, or appended
-## by defining e.g. AppendWaitMessages: [ ... ]
-## ... and remember, yamllint is your friend.
-AllowDirect: true
-Help:
-- Keywords: [ "draw", "image", "paint" ]
-  Helptext:
-  - "(bot), draw <description> - generate an image with OpenAI Dall-E"
-- Keywords: [ "ai", "prompt", "query" ]
-  Helptext:
-  - "(bot), prompt <query> - start a new threaded conversation with OpenAI"
-  - "(bot), p: <query> - shorthand for prompt"
-  - "(bot), r(egenerate) - re-send the previous prompt"
-  - "(bot), continue <follow up> - continue the conversation with the AI (threads only)"
-  - "(bot), c: <follow up> - shorthand for continue the conversation with the AI (threads only)"
-  - "(bot), ai <query> - send a single query to OpenAI, generating a reply in the channel"
-  - "(bot), add-token - add your personal OpenAI token (robot will prompt you in a DM)"
-  - "(bot), remove-token - remove your personal OpenAI token"
-  - "(bot), debug-ai - add debugging output during interactions"
-  - "(bot), ai-status - in a thread, give conversation status"
-CommandMatchers:
-- Command: 'prompt'
-  Regex: '(?i:p(?:rompt)?(?:=([\w-]+))?(?:/(debug)?)?[: ]\s*(.*))'
-- Command: 'debug'
-  Regex: '(?i:d(ebug[ -]ai)?)'
-- Command: 'status'
-  Regex: '(?i:ai[ -]status)'
-- Command: 'regenerate'
-  Regex: '(?i:r(egenerate|etry|epeat)?)'
-- Command: 'ai'
-  Regex: '(?i:ai(?:=([\w-]+)?(?:/(debug))?)?[: ]\s*(.*))'
-- Command: 'image'
-  Regex: '(?i:(?:draw|paint|image)\s*(.*))'
-- Command: 'continue'
-  Regex: '(?i:c(?:ontinue)?[: ]\s*(.*))'
-- Command: 'token'
-  Regex: '(?i:(?:link|add|set)[ -]token)'
-- Command: 'rmtoken'
-  Regex: '(?i:(?:rm|remove|unlink|delete|unset)[ -]token)'
-Config:
-## Generated with help from an earlier version of the plugin
-  WaitMessages:
-  - "please be patient while I contact the great mind of the web"
-  - "hold on while I connect to the all-knowing oracle"
-  - "just a moment while I get an answer from the digital diviner"
-  - "give me a second while I reach out to the cosmic connector"
-  - "stand by while I consult the infinite intelligence"
-  - "hang tight while I access the virtual visionary"
-  - "one moment while I check in with the omniscient overseer"
-  - "sit tight while I access the all-seeing sage"
-  - "wait here while I query the network navigator"
-  - "hang on while I communicate with the digital prophet"
-  - "wait here a moment while I talk to the universal wisdom"
-  - "just a sec while I reach out to the high-tech guru"
-  - "hold on a bit while I contact the technological titan"
-  - "be right back while I get an answer from the techno telepath"
-  DrawMessages:
-  - "give us a sec - our AI is brushing up on its drawing skills..."
-  - "hang tight - the AI is taking a moment to gather inspiration from its favorite memes"
-  - "chill for a moment - our AI is meditating on the perfect color scheme for your image"
-  - "please hold while the AI practices its signature for your image"
-  - "sit tight while our AI sharpens its pencils... metaphorically, of course"
-  - "hang on - the AI is taking a quick break to refuel on coffee and creativity"
-  - "one sec - our AI is warming up its digital paintbrush for your image"
-  - "please wait while the AI daydreams about your picture-perfect image"
-  - "hang on, our AI is putting on its creative thinking cap for your image"
-  - "please wait - the AI is doing a quick sketch of your image in its mind before getting started"
-  - "please hold while the AI takes a moment to visualize your masterpiece"
-  - "relax for a moment - our AI is doing some calisthenics to get pumped up for your image"
-  - "please join the AI in taking a deep breath - it's getting ready to bring your vision to life!"
-  - "please wait while the AI puts on some classical music to get in the zone"
-  Profiles:
-    "default":
-      "params":
-        "model": "gpt-3.5-turbo"
-        "temperature": 0.77
-      "system": |
-        You are ChatGPT, a large language model trained by OpenAI. Answer as correctly as possible.
-      "max_context": 3072
 DEFCONFIG
 
 case command
 when "init"
   exit(0)
 when "configure"
-  puts(defaultConfig)
+  # puts(defaultConfig)
   exit(0)
 end
 
-# success = bot.Exclusive("ai", false)
-# exit(0)
-
 direct = (bot.channel == "")
+cmdmode = ENV["GOPHER_CMDMODE"]
+
+# When command mode = "alias", reproduce the logic of builtin-fallback
+if command == "catchall" and cmdmode == "alias"
+  botalias = bot.GetBotAttribute("alias")
+  if direct
+    bot.Say("Command not found; try your command in a channel, or use '#{botalias}help'")
+  else
+    bot.SayThread("No command matched in channel '#{ENV["GOPHER_CHANNEL"]}'; try '#{botalias}help'")
+  end
+end
 
 case command
 # All the conversation commands
 # "catchall" for all messages sent to Floyd that didn't match other commands
 # "subscribed" for all messages in a subscribed thread
-# "continue" for an explicit continuation
-# "ai" for one-shot (non-continuing) queries
 # "regenerate" to resend the last query
 when "subscribed", "prompt", "ai", "continue", "regenerate", "catchall"
   bot.Log(:debug, "handling conversation command '#{command}' from #{ENV["GOPHER_USER"]}/#{ENV["GOPHER_USER_ID"]} in channel #{ENV["GOPHER_CHANNEL"]}/t:#{ENV["GOPHER_THREAD_ID"]}")
